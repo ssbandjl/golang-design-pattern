13aadd0af1f1c54b44d2e04cb098ee9b6136490d
xiaobing.song <xiaobing.song@cloudminds.com>
2021-01-13 09:56:35 +0800

    update, ref:https://github.com/ssbandjl/golang-design-pattern

--- gitpush.sh
+++ gitpush.sh
@@ -3,11 +3,11 @@
 [ $# -gt 0 ] && {
   commitInfo=$1
 }||{
-  commitInfo="update"
+  commitInfo='update, ref:https://github.com/ssbandjl/golang-design-pattern'
 }
 
 git pull
 git add .
-git commit -m $commitInfo
+git commit -m "$commitInfo"
 git push
 git log|head -n 20
